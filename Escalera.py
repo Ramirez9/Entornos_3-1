d2e74dea74d8a91eb4107cb90a2e369abf2ba7af
Ramirez9 <ramiirez100@gmail.com>
2017-04-28 12:59:33 +0200

    Modificacion de variables

--- Escalera.py
+++ Escalera.py
@@ -3,34 +3,34 @@ Created on 24 abr. 2017
 
 @author: neogeo39
 '''
-number_list = []
-number_list.extend([])
+numero_lista = []
+numero_lista.extend([])
 
 print "----Number Range----"
 
-number1 = int(input("Enter first number: "))
-number2 = int(input("Enter second number: "))
-number3 = int(input("Enter third number: "))
-if number1 == number2 or number2 == number3:
+numero = int(input("Enter first number: "))
+numero2 = int(input("Enter second number: "))
+numero3 = int(input("Enter third number: "))
+if numero == numero2 or numero2 == numero3:
     print "That's not a valid number"
 
 else:
     
-    if number1 > number2:
-        for i in reversed (range(number2, number1 + 1)):
-            number_list.extend([i])
+    if numero > numero2:
+        for i in reversed (range(numero2, numero + 1)):
+            numero_lista.extend([i])
     
     else :
-        for i in range(number1 + 1, number2 + 1):
-            number_list.extend([i - 1])
+        for i in range(numero + 1, numero2 + 1):
+            numero_lista.extend([i - 1])
             
-    if number2 > number3:
-        for b in reversed(range(number3, number2 + 1)):
-            number_list.extend([i + 1])
+    if numero2 > numero3:
+        for b in reversed(range(numero3, numero2 + 1)):
+            numero_lista.extend([i + 1])
     
     
     else :
-        for b in range (number2 + 1, number3 + 1):
-            number_list.extend([b])
+        for b in range (numero2 + 1, numero3 + 1):
+            numero_lista.extend([b])
             
-    print number_list
+    print numero_lista
